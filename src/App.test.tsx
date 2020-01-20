38b95072b039782ff7bc25782d378e574ccfe310
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2020-01-20 11:15:27 +0100

    :sparkles: connect App to Redux

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/index';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+const Container = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+test('renders GitGuardian App title', () => {
+  const { getByText } = render(Container);
+  const linkElement = getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Login } from './components/Login/Login';
+import { useSelector } from 'react-redux';
+import { IState } from './types/actions';
+import { Dashboard } from './components/Dashboard/Dashboard';
+
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const isConnected = useSelector((state: IState) => state.user.isConnected);
+  return !isConnected ? <Login />: <Dashboard />;
 }
 
 export default App;
