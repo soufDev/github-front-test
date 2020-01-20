4bfd20789adbfef9c62b828280e9ef8dca2fc39c
Soufiane AIT AKKACHE <ait.akkache.soufiane@gmail.com>
2020-01-20 10:58:06 +0100

    :sparkles: add theme file for styled components

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+export default {
+    white: '#fff',
+    black: '#111',
+    red: '#ff0000',
+    lightDark: '#30353A',
+    veryLightGray: '#EBEBEB',
+    focusYellow: '#F3C138',
+    yellow: '#FBC93F',
+    blue: '#157ACC',
+    errorMessageTextColor: '#a94442',
+    errorMessageBgColor: '#f2dede',
+    errorMessageBorderColor: '#ebccd1',
+    gray: '#666'
+}
